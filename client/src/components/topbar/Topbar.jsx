4843281bec983d21ac2b2d6ebff3c732ed3a914f
thadeus <thadeusnyariki94@gmail.com>
2023-03-27 09:45:24 +0300

    personal logo

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -8,7 +8,7 @@ const Topbar = () => {
       <div className="container">
         <div className="left">
           <div className="logo">
-            <img src="/img/logo.png" alt="" />
+            <img src="/img/thadeusdev-high-resolution-logo-black-on-transparent-background.png" alt="" />
           </div>
           <div className="search">
             <input type="text" placeholder='Search' />
