56ba244b50b2e7a5fee5a5c4c5f9f739bb1dcab5
thadeus <thadeusnyariki94@gmail.com>
2023-04-09 18:04:21 +0300

    search

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,9 +1,24 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import './Topbar.scss'
 import { AiOutlineSearch, AiOutlineLogout } from "react-icons/ai";
-import Alarms from '../../pages/alarms/Alarms';
+import axios from 'axios'
 
 const Topbar = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+    if (event.target.value.length > 2) {
+      axios
+        .get(`?q=${event.target.value}`)
+        .then((response) => setResults(response.data))
+        .catch((error) => console.error(error));
+    } else {
+      setResults([]);
+    }
+  };
+
   return (
     <div className='topbar'>
       <div className="container">
@@ -12,7 +27,7 @@ const Topbar = () => {
             <img src="/img/logo.png" alt="" />
           </div>
           <div className="search">
-            <input type="text" placeholder='Search...' />
+            <input type="text" placeholder='Search...' value={query} onChange={handleChange} results={results} />
             <AiOutlineSearch className='icon' />
           </div>
         </div>
